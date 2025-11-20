5d18b820dfce3599d4d2888eef86dd82d290735a
Nihir Verma <nihirverma14@gmail.com>
2025-11-20 22:50:48 +0530

    first commit

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,13 @@
+import SimpleCard from './components/SimpleCard'
+
+const App = () => {
+  return (
+    <div>
+      <div className="parent">
+        <SimpleCard />
+      </div>
+    </div>
+  )
+}
+
+export default App
